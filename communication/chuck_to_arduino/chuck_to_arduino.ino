60021346afd3b44bb1aff6644778889b013772ac
Eric Heep <ericheep@gmail.com>
2014-12-16 23:38:34 -0600

    separated methods into separate functions, still working on bit unpacking for frequency

--- communication/chuck_to_arduino/chuck_to_arduino.ino
+++ communication/chuck_to_arduino/chuck_to_arduino.ino
@@ -3,7 +3,7 @@
 // recieves messages from ChucK to send to piezo speakers
 
 // ID number of the arduino, each robot must have a different one
-#define arduinoID 6
+#define arduinoID 3
 
 // used to pair arduinos together
 int handshake;
@@ -17,27 +17,62 @@ long long frq;
 
 void setup() {
   Serial.begin(9600);
-}
 
-void loop() {
-  if (Serial.available()) {
-    // reads in a four element array from ChucK
-    Serial.readBytes(bytes, 4);
-
-    // bit unpacking
-    num = byte(bytes[0]) >> 4;
-    frq = (byte(bytes[0]) << 24 | byte(bytes[1]) << 16 | byte(bytes[2]) << 8 | byte(bytes[3])) & 134217727; 
-
-    // message required for "handshake" to occur
-    if (handshake == 0 && num == 15 && frq == 134217727) { 
-      Serial.write(arduinoID);
-      handshake = 1;
-    }
-    else {
-      // will write ti piezos here
-      // -------------------------
-      // num
-      // frq * 0.001
-    }
+  // LED testing
+  for (int i = 0; i < 3; i++) {
+    pinMode(11 + i, OUTPUT);
   }
 }
+
+// main program
+void loop() {
+  if (Serial.available() && handshake == 1) {
+    unpack();
+  }
+  else if (Serial.available() && handshake == 0) {
+    sendID();
+  }
+}
+
+void unpack() {
+  // reads in a four element array from ChucK
+  Serial.readBytes(bytes, 4);
+
+  // bit unpacking
+  num = byte(bytes[0]) >> 3;
+  //frq = (byte(bytes[0]) << 24 | byte(bytes[1]) << 16 | byte(bytes[2]) << 8 | byte(bytes[3])) & 134217727; 
+  frq = byte(bytes[2] << 8); 
+  digitalWrite(12, HIGH);
+  delay(50);
+  digitalWrite(12, LOW); 
+  
+  if (frq == 59904) {
+    digitalWrite(11, HIGH);
+    delay(50);
+    digitalWrite(11, LOW); 
+  }
+  // will write to piezos here
+  // -------------------------
+  // num
+  // frq * 0.001 
+}
+
+// intializes communication
+void sendID() {
+  char initialize[2];
+  Serial.readBytes(initialize, 2);
+
+  if (byte(initialize[0]) == 255 && byte(initialize[1]) == 255) { 
+    Serial.write(arduinoID);
+    handshake = 1;
+  }
+}
+
+
+
+
+
+
+
+
+
