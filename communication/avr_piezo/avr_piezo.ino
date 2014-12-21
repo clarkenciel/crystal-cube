ab7342ca6ed28df9a593a957129da264ee968312
Eric Heep <ericheep@gmail.com>
2014-12-21 13:32:17 -0600

    concurrency with three piezos

--- communication/avr_piezo/avr_piezo.ino
+++ communication/avr_piezo/avr_piezo.ino
@@ -4,21 +4,26 @@
 // setting 
 #define PIN_DDR DDRD
 #define PIN_PORT PORTD
+#define NUM_PIEZOS 3
 
-double sr = 62500;
-double frq = 440.0;
-double cycles;
-double phase;
-double phase_inc;
+uint8_t pin_map;
+
+double sr = 15625;
+double frq[NUM_PIEZOS];
+double cycles[NUM_PIEZOS];
+double phase[NUM_PIEZOS];
+double phase_inc[NUM_PIEZOS];
 
 // delicious pi
 double pi = 3.14159265359;
 double two_pi = pi * 2;
 
 void setup() {  
-  frq = 880;
-  cycles = frq / sr;
-  phase_inc = cycles * two_pi;
+  for (int i = 0; i < NUM_PIEZOS; i++) {
+    frq[i] = 330 * (i + 1);
+    cycles[i] = frq[i] / sr;
+    phase_inc[i] = cycles[i] * two_pi;
+  }
 
   // making my LEDs all ready
   PIN_DDR |= (1 << DDB0)|(1 << DDB1)|(1 << DDB2)|(1 << DDB3)|(1 << DDB4)|(1 << DDB5)|(1 << DDB6)|(1 << DDB7);
@@ -33,13 +38,13 @@ void setup() {
   TCCR0A = 0;
   TCCR0B = 0;
   TCNT0  = 0;
-  
+
   // set compare match register for 2khz increments
   OCR0A = 1;// = (16*10^6) / (20000*64) - 1 (must be <256)
   // turn on CTC mode
   TCCR0A |= (1 << WGM01);
   // Set CS12 bit for 256 prescaler
-  TCCR0B |= (1 << CS12);   
+  TCCR0B |= (1 << CS12) | (1 << CS10);   
   // enable timer compare interrupt
   TIMSK0 |= (1 << OCIE0A);
 
@@ -49,20 +54,20 @@ void setup() {
 
 // loops at 2 millionths of a second (I wish, still figuring out timing)
 ISR(TIMER0_COMPA_vect){
+  pin_map = 0;
+  for (int i = 0; i < NUM_PIEZOS; i++) {
+    phase[i] += phase_inc[i];
 
-  phase += phase_inc;
-
-  if (phase <= pi) {
-    PIN_PORT = B11111111;
+    if (phase[i] <= pi) {
+      pin_map = pin_map | 1 << (i + 2);
+    }
+    
+    // wrap
+    if (phase[i] >= two_pi) {
+      phase[i] -= two_pi; 
+    }
   }
-  else {
-    PIN_PORT = 0; 
-  }
-
-  // wrapp
-  if (phase >= two_pi) {
-    phase -= two_pi; 
-  }
+  PIN_PORT = pin_map;
 }
 
 // this loop just HAS to be here
@@ -70,6 +75,10 @@ void loop() {
   Serial.print("!");
   // boring loop is boring
 }
+
+
+
+
 
 
 
