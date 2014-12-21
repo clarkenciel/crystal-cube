d31d5497b53dc0885fef7434a260813f4532f5bd
Eric Heep <ericheep@gmail.com>
2014-12-20 19:13:12 -0600

    added naive frequency system

--- communication/avr_piezo/avr_piezo.ino
+++ communication/avr_piezo/avr_piezo.ino
@@ -10,8 +10,8 @@ uint8_t inc;
 uint8_t num;
 uint8_t val;
 
-void setup() {
-  val = 5000;
+void setup() {  
+  num = 5;
   
   // making my LEDs all ready
   PIN_DDR |= (1 << DDB0)|(1 << DDB1)|(1 << DDB2)|(1 << DDB3)|(1 << DDB4)|(1 << DDB5)|(1 << DDB6)|(1 << DDB7);
@@ -24,13 +24,13 @@ void setup() {
   TCCR1B = 0;
   TCNT1  = 0;
   // setting reset clock to 65535 (max for timer 1, which is a 16bit clock)
-  OCR1A = 65535;
+  OCR1A = 249;
   // turn on CTC mode
   TCCR1B |= (1 << WGM12);
   // set clk/8
-  TCCR1B |= (1 << CS11);  
+  // TCCR1B |= (1 << CS11);  
   // set clk/256
-  //TCCR1B |= (1 << CS12);  
+  TCCR1B |= (1 << CS12);  
   // enable timer compare interrupt
   TIMSK1 |= (1 << OCIE1A);
 
